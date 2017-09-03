8b43084216d45af20de6275306a0f54569e09159
seasail <seasail@desktop-miaoyh>
2017-09-03 19:46:26 +0800

    xz 环境

--- framework-parent/framework-api/src/main/java/web/framework/api/BootApplication.java
+++ framework-parent/framework-api/src/main/java/web/framework/api/BootApplication.java
@@ -5,17 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import web.framework.java.base.BaseService;
+
 
 @SpringBootApplication
 @ServletComponentScan
 public class BootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootApplication.class, args);
+//        SpringApplication.run(BootApplication.class, args);
         
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "dubbo-consumer.xml" });
         context.start();
         
         BaseService baseService = (BaseService) context.getBean("baseService");
+        String sss = baseService.demo("ttt");
+        System.out.println(sss);
     }
 }
